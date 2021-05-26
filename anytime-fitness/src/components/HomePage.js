b4e9399b86e3e63dfded3eefa3869726addc8cea
thecuriousones <tamaraleonard46@gmail.com>
2021-05-26 15:04:01 -0400

    done with homepage styling signup forms now

--- anytime-fitness/src/components/HomePage.js
+++ anytime-fitness/src/components/HomePage.js
@@ -18,36 +18,45 @@ function HomePage() {
         <div className='home'>
             <div class="container">
                 <div class="nav">
-                    <div class="logo">Tripp.</div>
+                    <div class="logo">HEALTH</div>
                     <ul>
                         <li>Download App</li>
-                        <li>Login</li>
-                        <li>SignUp</li>
-                        <li>Help</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li><Link to='/login' style={{  textDecoration: 'none' }}>Sign in</Link></li>
                     </ul>
                 </div>
             </div>
 
             <div class="hero">
-                <div class="title">Argentina</div>
+                <div class="title">Anywhere Fitness</div>
 
-                <div class="button">
-                    explore
+                <div className='home-buttons'>
+                    <div className='home-button1' onClick={routeToClientSignUp}>
+                        Create Client Account
+                    </div>
+                    <div className='home-button2' onClick={routeToInstructorSignUp}>
+                        Create Instructor Account
+                    </div>
                 </div>
             </div>
+            <div className='home-link'>
+                <Link to='/login' style={{ color: 'black', textDecoration: 'none' }}>or click here to sign in</Link>
+            </div>
             <div className='space'>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
-               <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
+                <div>space</div>
             </div>
         </div>
     )
@@ -55,29 +64,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-{/* <div className='home'>
-    <nav className='nav-container'>
-        <h1></h1>
-        <div className='nav-links'>
-            <Link to='/login'>Sign in</Link>
-        </div>
-    </nav>
-    <h1>Anytime Fitness</h1>
-    <div className='home-wrapper'>
-        <img
-            className='home-image'
-            src='https://i.stack.imgur.com/y9DpT.jpg'
-            alt='place-holder.img'
-        />
-    </div>
-    <div className='buttons-and-link'>
-        <button className='clientButton' onClick={routeToClientSignUp}>
-            Create Client Account
-                </button>
-        <button className='instructorButton' onClick={routeToInstructorSignUp}>
-            Create Instructor Account
-                </button>
-        <Link to='/login'>or click here to sign in</Link>
-    </div>
-</div> */}
