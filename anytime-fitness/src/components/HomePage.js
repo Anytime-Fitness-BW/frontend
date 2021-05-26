c8ca29fe06628a7de07d94a1586602d63bfefc58
thecuriousones <tamaraleonard46@gmail.com>
2021-05-26 03:35:19 -0400

    will finish styling homepage later

--- anytime-fitness/src/components/HomePage.js
+++ anytime-fitness/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory, Link } from "react-router-dom";
+import './Home.css'
 
 function HomePage() {
     const history = useHistory();
@@ -14,29 +15,39 @@ function HomePage() {
 
 
     return (
-        <div>
-            <nav>
-                <h1></h1>
-                <div className='nav-links'>
-                    <Link to='/login'>Sign in</Link>
+        <div className='home'>
+            <div class="container">
+                <div class="nav">
+                    <div class="logo">Tripp.</div>
+                    <ul>
+                        <li>Download App</li>
+                        <li>Login</li>
+                        <li>SignUp</li>
+                        <li>Help</li>
+                    </ul>
                 </div>
-            </nav>
-            <h1>Anytime Fitness</h1>
-            <div className='home-wrapper'>
-                <img
-                    className='home-image'
-                    src='https://i.stack.imgur.com/y9DpT.jpg'
-                    alt='place-holder.img'
-                />
             </div>
-            <div className='buttons-and-link'>
-                <button className='clientButton' onClick={routeToClientSignUp}>
-                    Create Client Account
-                </button>
-                <button className='instructorButton' onClick={routeToInstructorSignUp}>
-                    Create Instructor Account
-                </button>
-                <Link to='/login'>or click here to sign in</Link>
+
+            <div class="hero">
+                <div class="title">Argentina</div>
+
+                <div class="button">
+                    explore
+                </div>
+            </div>
+            <div className='space'>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
+               <div>space</div>
             </div>
         </div>
     )
@@ -44,3 +55,29 @@ function HomePage() {
 }
 
 export default HomePage;
+
+{/* <div className='home'>
+    <nav className='nav-container'>
+        <h1></h1>
+        <div className='nav-links'>
+            <Link to='/login'>Sign in</Link>
+        </div>
+    </nav>
+    <h1>Anytime Fitness</h1>
+    <div className='home-wrapper'>
+        <img
+            className='home-image'
+            src='https://i.stack.imgur.com/y9DpT.jpg'
+            alt='place-holder.img'
+        />
+    </div>
+    <div className='buttons-and-link'>
+        <button className='clientButton' onClick={routeToClientSignUp}>
+            Create Client Account
+                </button>
+        <button className='instructorButton' onClick={routeToInstructorSignUp}>
+            Create Instructor Account
+                </button>
+        <Link to='/login'>or click here to sign in</Link>
+    </div>
+</div> */}
